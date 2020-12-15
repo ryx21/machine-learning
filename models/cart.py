f60792a5fb3c2b0dd07c5a80be999885a6b87a7d
Rory Xiao <rory@MacBook-Pro.local>
2020-12-15 22:41:10 +0000

    refactoring

--- models/cart.py
+++ models/cart.py
@@ -1,4 +1,5 @@
-from models.helpers import *
+import numpy as np
+from models.helpers import find_best_split_for_feature, get_label_probabilities, gini_impurity
 
 # TODO: implement min_samples_leaf
 # TODO: implement alternative splitting functions
@@ -7,15 +8,23 @@ from models.helpers import *
 
 class CART:
 
-    def __init__(self, n_classes, depth=0, **kwargs):
+    def __init__(self,
+                 n_classes,
+                 depth=0,
+                 max_depth=np.infty,
+                 min_samples_leaf=1,
+                 min_samples_split=2,
+                 split_criterion="gini",
+                 max_feature_ratio=1,
+                 ):
         self._n_classes = n_classes
         self._depth = depth
-        # set up kwargs
-        self._config = kwargs
-        self._max_depth = kwargs.get('max_depth', np.infty)
-        self._min_samples_leaf = kwargs.get('min_samples_leaf', 1)
-        self._min_samples_split = kwargs.get('min_samples_split', 2)
-        self._impurity_function = kwargs.get('impurity_function', gini_impurity)
+        # set up parameters
+        self._max_depth = max_depth
+        self._min_samples_leaf = min_samples_leaf
+        self._min_samples_split = min_samples_split
+        self._split_criterion = split_criterion
+        self._max_feature_ratio = max_feature_ratio
         # node information
         self._leaf_class = None
         self._is_leaf_node = False
@@ -23,6 +32,27 @@ class CART:
         self._right_child = None
         self._feature_index = None
         self._feature_threshold = None
+        # assert all arguments are allowable
+        self._check_arguments()
+        # setup impurity function
+        self._impurity_function = self._get_impurity_function(split_criterion)
+
+    def _check_arguments(self):
+        assert self._n_classes > 1, "'n_classes' must be greater than 1"
+        assert self._max_depth > 0, "'max_depth' must be greater than 0"
+        assert self._min_samples_leaf > 0, "'min_samples_leaf' must be greater than 0"
+        assert self._min_samples_split > 1, "'min_samples_split' must be greater than 1"
+        assert (1 >= self._max_feature_ratio > 0), "'max_feature_ratio' must be within [0, 1)"
+        assert self._split_criterion in ['gini', 'entropy', 'misclassification'],\
+            "'split_criterion' must be one of: gini, entropy, misclassification'"
+
+    def _get_impurity_function(self, impurity_function):
+        if impurity_function == "gini":
+            return gini_impurity
+        elif impurity_function == "entropy":
+            raise NotImplementedError
+        elif impurity_function == "misclassification":
+            raise NotImplementedError
 
     def _find_optimum_split(self, data, labels):
         # data (#samples, #features), labels (#samples)
@@ -47,6 +77,18 @@ class CART:
         self._is_leaf_node = True
         self._leaf_class = np.argmax(get_label_probabilities(labels, self._n_classes))
 
+    def _create_child(self):
+        child_node = CART(
+            self._n_classes,
+            self._depth + 1,
+            self._max_depth,
+            self._min_samples_leaf,
+            self._min_samples_split,
+            self._split_criterion,
+            self._max_feature_ratio
+        )
+        return child_node
+
     def _stop_condition(self, data, labels):
         # check if fewer than required number of samples per leaf
         cond1 = len(data) < self._min_samples_split
@@ -63,10 +105,10 @@ class CART:
             # split data and creates left and right child nodes
             mask = data[:, self._feature_index] <= self._feature_threshold
             # create and fit left child node
-            self._left_child = CART(self._n_classes, self._depth + 1, **self._config)
+            self._left_child = self._create_child()
             self._left_child.fit(data[mask, :], labels[mask])
             # create and fit right child node
-            self._right_child = CART(self._n_classes, self._depth + 1, **self._config)
+            self._right_child = self._create_child()
             self._right_child.fit(data[~mask, :], labels[~mask])
         else:
             self._create_leaf(labels)
